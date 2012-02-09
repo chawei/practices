2732e942ccb0734a7d17487cef953a9dc8cd4c06
david hsu <chawei.hsu@gmail.com>
2012-02-09 10:30:54 -0800

    refactor sqrt function

--- sqrt.rb
+++ sqrt.rb
@@ -2,23 +2,15 @@ def sqrt(num, precision = 0.01)
   upper_num = num
   lower_num = 0
   
-  guess_num        = (upper_num + lower_num)/2.0
-  sqr_of_guess_num = sqr_of_num(guess_num)
-  while (sqr_of_guess_num - num).abs > precision
+  begin
+    guess_num        = (upper_num + lower_num)/2.0
+    sqr_of_guess_num = guess_num ** 2
     if sqr_of_guess_num > num
       upper_num = guess_num
     else
       lower_num = guess_num
     end
-    
-    guess_num        = (upper_num + lower_num)/2.0
-    sqr_of_guess_num = sqr_of_num(guess_num)
-  end
-  
+  end while (sqr_of_guess_num - num).abs > precision
+
   return ((1/precision) * guess_num).round * precision
-end
-
-
-def sqr_of_num(num)
-  num ** 2
 end
